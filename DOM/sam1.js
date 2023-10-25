42809ab3753674ead7f292fdd3456c373ad5360d
SamreenBegum176 <samreenbegum515>
2023-10-25 10:02:18 -0700

    Making the 2nd item Green Color background and Making 3rd item invisible

--- DOM/sam1.js
+++ DOM/sam1.js
@@ -17,7 +17,7 @@ for(var i=0;i<items.length;i++){
 }*/
 
 // GET ELEMENT BY TAGNAME//
-var li=document.getElementsByTagName('li');
+/*var li=document.getElementsByTagName('li');
 console.log(li);
 console.log(li[1]);
 li[1].textContent = 'Hello 2';
@@ -26,4 +26,43 @@ li[1].style.backgroundColor = 'yellow';
 
 for(var i=0;i<li.length;i++){
     li[i].style.backgroundColor = '#f4f4f4';
-}
+}*/
+
+// QUERY SELECTOR //
+/*var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4p #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value="SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color='red';
+
+var lastitem = document.querySelector('.list-group-item:last-child');
+lastitem.style.color='blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color='coral';*/
+
+// QUERYSELECTORALL //
+/*var titles = document.querySelectorAll('.title');
+console.log(titles);
+
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}*/
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display="none";
