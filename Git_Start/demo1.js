dc26b8edc631d7ad8f037cba474e537fa068cf87
SamreenBegum176 <samreenbegum515>
2023-10-25 07:10:31 -0700

    Adding_JavaScript_DOM

---
+++ Git_Start/demo1.js
@@ -0,0 +1,38 @@
+//Eamine the Document object//
+//console.dir(document);//shows us all of the different properties and methods attaced to the document object
+
+//console.dir(document);
+/*console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+//document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+//document.all[10].textContent='Hello';
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);*/
+
+//GET ELEMENT BY ID//
+
+//console.log(document.getElementById('header-title'));
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='Hello';
+//headerTitle.innerText='Goodbye';
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML = '<h3>Hello</h3>';
+//header.style.borderBottom = 'solid 3px #000';
+
+
+var itemElement = document.getElementById("item");
+
+itemElement.style.fontWeight = "bold";
+itemElement.style.color = "green";
+
+
+
